b02cd369f689ccfe4ee61655ce23d4cb3f4e41bc
Tom Krüger <tom@joloto.de>
2023-02-10 16:34:35 +0100

    better comments

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -14,16 +14,22 @@ int Channel::add_member( User& new_member, bool is_invitation, std::string key =
     // irc_log(trace, "Channel::add_member(): ", new_member.get_nick());
 
     if (find_member(new_member.get_nick()) != m_members.end())
-        return ERR_USERONCHANNEL; // useronchannel
+    {
+        return ERR_USERONCHANNEL;
+    }
     else if (m_flags & MBR_LIMIT_CHANNEL_FLAG && m_members.size() >= m_member_limit)
-        return ERR_CHANNELISFULL; // channelisfull
-    else if ((m_flags & PRIVATE_CHANNEL_FLAG
-                || m_flags & INV_ONLY_CHANNEL_FLAG) && is_invitation == false)
-        return ERR_INVITEONLY; // private channel or inviteonly
+    {
+        return ERR_CHANNELISFULL;
+    }
+    else if ((m_flags & PRIVATE_CHANNEL_FLAG || m_flags & INV_ONLY_CHANNEL_FLAG)
+                && is_invitation == false)
+    {
+        return ERR_INVITEONLY;
+    }
     else if (m_flags & SET_KEY_CHANNEL_FLAG && is_invitation == false)
     {
         if (key.empty() || key != m_channel_key)
-            return ERR_BADCHANNELKEY; // badchannelkey
+            return ERR_BADCHANNELKEY;
     }
 
     broadcast(build_join_channel(new_member.get_fd(), new_member, m_name), m_members.end());
@@ -44,17 +50,17 @@ int Channel::invite_user(User& user, std::string& caller)
 
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     if (!is_chanop(m_it))
-        return ERR_CHANOPRIVSNEEDED; // chanoprivsneeded
+        return ERR_CHANOPRIVSNEEDED;
 
     if (m_flags & MBR_LIMIT_CHANNEL_FLAG && m_members.size() >= m_member_limit)
-        return ERR_CHANNELISFULL; // channelisfull
+        return ERR_CHANNELISFULL;
 
     int err = add_member(user, true);
     if (err != 0)
-        return ERR_USERONCHANNEL; // useronchannel
+        return ERR_USERONCHANNEL;
     
     rpl_inviting(m_it->first.get_fd(), m_it->first.get_nick(), m_name, user.get_nick());
     return 0;
@@ -78,13 +84,13 @@ int Channel::remove_member( std::string& caller, std::string& msg )
 
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     broadcast(build_part_channel(m_it->first.get_fd(), m_it->first, m_name, msg), m_it);
 
     m_members.erase(m_it);
     if (m_members.size() == 0)
-        return -1; // channel will be destroyed
+        return -1; // channel empty, will be destroyed
     if (num_of_chanops() == 0)
     {
         m_members.front().second.chanop = 1; // new chanop needs to know that he became chanop
@@ -99,16 +105,18 @@ int Channel::kick_member( std::string& caller, std::string& nick, std::string& m
 
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     if (is_chanop(m_it) == 0)
-        return ERR_CHANOPRIVSNEEDED; // chanoprivsneeded
+        return ERR_CHANOPRIVSNEEDED;
 
-    int err = remove_member(nick, message);
-    if (err == ERR_NOTONCHANNEL)
-        return ERR_USERNOTINCHANNEL; // usernotinchannel (needs to be converted from ERR_NOTONCHANNEL to ERR_USERNOTINCHANNEL cause we change the perspective from one member to another)
-    else if (err == -1)
+    switch (remove_member(nick, message))
+    {
+    case ERR_NOTONCHANNEL:
+        return ERR_USERNOTINCHANNEL; // conversion from ERR_NOTONCHANNEL to ERR_USERNOTINCHANNEL cause we change the perspective from one member to another
+    case -1:
         return -1; // channel empty, will be destroyed
+    }
 
     return 0;
 }
@@ -119,10 +127,10 @@ int Channel::channel_message( std::string& caller, std::string& msg )
 
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     if ((m_flags & MODERATED_CHANNEL_FLAG && !m_it->second.speaker) && !is_chanop(m_it))
-        return ERR_CANNOTSENDTOCHAN; // cannotsendtochan
+        return ERR_CANNOTSENDTOCHAN;
 
     broadcast(msg, m_it);
     return 0;
@@ -132,7 +140,7 @@ int Channel::topic( std::string& caller )
 {
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     if (m_topic.empty())
         rpl_notopic(m_it->first.get_fd(), m_name);
@@ -148,12 +156,12 @@ int Channel::set_topic( std::string& caller, std::string& new_topic )
 
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     if (m_flags & TPC_CHANOP_CHANNEL_FLAG && !is_chanop(m_it))
-        return ERR_CHANOPRIVSNEEDED; // chanoprivsneeded
+        return ERR_CHANOPRIVSNEEDED;
 
-    m_topic = new_topic.substr(1);
+    m_topic = new_topic.substr(1); // skipping the double colon
     return 0;
 }
 
@@ -164,10 +172,10 @@ int Channel::member_modes( std::string& caller, StringVector& target_nick, Strin
 
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     if (is_chanop(m_it) == 0)
-        return ERR_CHANOPRIVSNEEDED; // chanoprivsneeded
+        return ERR_CHANOPRIVSNEEDED;
 
     if (target_nick.empty()) // problem: if there is an unknown flag char, we will only send err_unknownmode when target_nick is not empty
         return ERR_NEEDMOREPARAMS;
@@ -179,13 +187,13 @@ int Channel::member_modes( std::string& caller, StringVector& target_nick, Strin
     for (StringVector::iterator arg_it = args.begin(); arg_it != args.end(); arg_it++)
     {
         if (arg_it->length() != 2)
-            return ERR_UNKNOWNMODE; // command too short
+            return ERR_UNKNOWNMODE;
         if (arg_it->at(1) == 'o')
             target_it->second.chanop = (arg_it->at(0) == '+');
         else if (arg_it->at(1) == 'v')
             target_it->second.speaker = (arg_it->at(0) == '+');
         else
-            return ERR_UNKNOWNMODE; // command not found
+            return ERR_UNKNOWNMODE;
 
         broadcast(rpl_channelmodeis(m_it->first.get_fd(), m_name, *arg_it, target_it->first.get_nick()), m_it);
     }
@@ -199,15 +207,15 @@ int Channel::channel_modes( std::string& caller, StringVector& args, StringVecto
 
     Members::iterator m_it = find_member(caller);
     if (m_it == m_members.end())
-        return ERR_NOTONCHANNEL; // notonchannel
+        return ERR_NOTONCHANNEL;
 
     if (is_chanop(m_it) == 0)
-        return ERR_CHANOPRIVSNEEDED; // chanoprivsneeded
+        return ERR_CHANOPRIVSNEEDED;
 
     for (StringVector::iterator arg_it = args.begin(); arg_it != args.end(); arg_it++)
     {
         if (arg_it->length() != 2)
-            return ERR_UNKNOWNMODE; // command too short
+            return ERR_UNKNOWNMODE;
         if (arg_it->at(1) == 'i')                           // invite only channel
             change_modes(*arg_it, INV_ONLY_CHANNEL_FLAG);
         else if (arg_it->at(1) == 't')                      // only channel operators can change the channels topic
@@ -227,17 +235,17 @@ int Channel::channel_modes( std::string& caller, StringVector& args, StringVecto
         else if (arg_it->at(1) == 'k')                      // channel key
         {
             if (params.empty() && m_channel_key.empty())
-                return ERR_NEEDMOREPARAMS; // needmoreparams
+                return ERR_NEEDMOREPARAMS;
             else if (!params.empty())
                 m_channel_key = params.front();
             else if (!params.empty() && !m_channel_key.empty())
-                return ERR_KEYSET; // keyset
+                return ERR_KEYSET;
             change_modes(*arg_it, SET_KEY_CHANNEL_FLAG);
             broadcast(rpl_channelmodeis(m_it->first.get_fd(), m_name, *arg_it, m_channel_key), m_it);
             break ;
         }
         else
-            return ERR_UNKNOWNMODE; // command not found
+            return ERR_UNKNOWNMODE;
 
         broadcast(rpl_channelmodeis(m_it->first.get_fd(), m_name, *arg_it, ""), m_it);
     }
